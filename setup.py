36314c206b41ba67b25ca1bc200137ba5340d156
GeekROS <15300555770>
2023-09-20 18:29:39 +0800

    1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="armcnc",
-    version="1.0.3",
+    version="1.0.4",
     author="MakerYang",
     author_email="admin@wileho.com",
     description="Python development framework for armcnc.",
