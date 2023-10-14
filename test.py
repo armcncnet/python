70fa4cdcedc019990d7ecfc5cc91d93fda1966ca
GeekROS <15300555770>
2023-10-14 22:04:28 +0800

    1.0.4

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from armcnc import Init
+
+# armcnc启动回调函数
+def armcnc_start(cnc):
+    while True:
+        pass
+
+def armcnc_message(cnc, message):
+    pass
+
+def armcnc_exit(cnc):
+    pass
+
+if __name__ == '__main__':
+    Init()
