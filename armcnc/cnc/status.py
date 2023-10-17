371b05475a0a06f64b32779398a2b8feca6a5ccf
GeekROS <15300555770>
2023-10-17 16:56:09 +0800

    1.0.5

--- armcnc/cnc/status.py
+++ armcnc/cnc/status.py
@@ -22,7 +22,7 @@ class Status:
             if self.framework.machine.is_alive:
                 try:
                     status = self.api.poll()
-                    print(getattr(status, "ini_filename"))
+                    print(status)
                     # print(status.ini_filename)
                 except linuxcnc.error as detail:
                     self.framework.utils.service.service_write({"command": "launch:error", "message": detail, "data": False})
