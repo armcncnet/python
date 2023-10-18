041b47daf6e51f96a842fc688197ef3dc492d9a3
GeekROS <15300555770>
2023-10-18 09:14:08 +0800

    1.0.5

--- armcnc/cnc/status.py
+++ armcnc/cnc/status.py
@@ -30,8 +30,6 @@ class Status:
                     if not x.startswith("_") and not callable(getattr(self.api, x)):
                         self.framework.machine.stat[x] = getattr(self.api, x)
 
-                self.framework.utils.json.dumps(self.framework.machine.stat)
-
-                print(self.framework.machine.stat)
+                self.framework.utils.service.service_write({"command": "launch:machine:status", "message": "", "data": self.framework.machine.stat})
 
             self.framework.utils.set_sleep(5)
