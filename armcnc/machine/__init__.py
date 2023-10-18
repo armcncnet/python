26719331167f7008af1018e72057875822b9a281
GeekROS <15300555770>
2023-10-18 10:51:12 +0800

    1.0.5

--- armcnc/machine/__init__.py
+++ armcnc/machine/__init__.py
@@ -23,6 +23,5 @@ class Machine:
             if self.is_alive:
                 if self.stat and self.stat["ini_filename"] != "":
                     inifile = linuxcnc.ini(self.stat["ini_filename"])
-                    for x in dir(inifile):
-                        print(x)
+                    print(inifile.find("EMC", "MACHINE"))
             self.framework.utils.set_sleep(0.2)
