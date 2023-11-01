0219f01230de8b9df29e1346d4d53f3e01e74606
GeekROS <15300555770>
2023-11-01 16:46:40 +0800

    1.0.147

--- armcnc/cnc/status.py
+++ armcnc/cnc/status.py
@@ -101,7 +101,7 @@ class Status:
                         g92_offset = g92_offset_tmp[axis_num]
                         g92_offset = "{:.3f}".format(g92_offset)
                         user_data["data"]["g92_offset"][i] = g92_offset
-                        user_data["data"]["velocity"][i] = int(axis_tmp[i]['velocity'])
+                        user_data["data"]["velocity"][i] = axis_tmp[i]["velocity"]
 
                     if user_data["task_state"] == 4:
                         self.father.framework.machine.task_state = True
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="armcnc",
-    version="1.0.146",
+    version="1.0.147",
     author="MakerYang",
     author_email="admin@wileho.com",
     description="Python development framework for armcnc.",
