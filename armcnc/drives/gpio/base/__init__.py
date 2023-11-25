39dc06a0f97658290c689db5eae232480e1cffbb
GeekROS <15300555770>
2023-11-25 11:32:12 +0800

    1.0.195

--- armcnc/drives/gpio/base/__init__.py
+++ armcnc/drives/gpio/base/__init__.py
@@ -12,14 +12,17 @@ class Base:
 
     def __init__(self, father):
         self.father = father
-        self.hal = hal
+        self.hal = None
         self.gpio = GPIO
 
     def setup(self):
         if self.father.coordinates != "" and self.father.machine.machine_path != "":
             self.gpio.setmode(GPIO.BCM)
+            self.hal = hal.component("armcncio")
+            self.hal.newpin("gpio.estop", hal.HAL_BIT, hal.HAL_IN)
+            self.hal.ready()
 
     def loop(self):
-        if self.father.coordinates != "" and self.father.machine.machine_path != "":
+        if self.hal and self.father.coordinates != "" and self.father.machine.machine_path != "":
             time.sleep(0.01)
 
